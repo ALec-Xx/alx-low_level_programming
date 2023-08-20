4a4c2189c39924470c9b0413eaab2d514192cfe1
ALec-Xx <alecsouthampton@gmail.com>
2023-08-20 00:29:50 +0000

    Quick fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - prints numbers, followed by a new line.
- * @seperator: string to be printed between numbers
+ * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
  * @...: variable arguments
  * Return: void
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -22,4 +22,10 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+
+void print_int(va_list *);
+void print_char(va_list *);
+void print_str(va_list *);
+void print_float(va_list *);
+
 #endif
