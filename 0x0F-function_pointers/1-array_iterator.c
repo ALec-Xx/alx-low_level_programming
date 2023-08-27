ad6d7733e84059336a212d81cc42dcfe8fb1d689
ALec-Xx <alecsouthampton@gmail.com>
2023-08-27 20:42:02 +0000

    Quick Fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 
 
 /**
- * void array_iterator - executes a function given as a parameter
+ * array_iterator - executes a function given as a parameter
  * on each element of an array.
  * @array: pointer to array of numbers
  * @size: number of elements in array
@@ -13,6 +13,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
+	if (!array || !action)
+		return;
 	while (array != NULL && i < size)
 	{
 		action(array[i]);
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * main - performs simple arithmetic
+ * @argc: number of arguments
+ * @argv: value of arguments
+ * Return: result int
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	op_t ptr;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	ptr.f = get_op_func(argv[2]);
+
+	if (ptr.f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = ptr.f(a, b);
+
+	if (result == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", result);
+
+	return (0);
+}
